9b88746e54c6a1be0b292df527f89d9e6e99877c
SaintEfim <saintefim2@gmail.com>
2024-07-27 18:36:25 +0300

    fixed automapper

--- src/WC.Service.PersonalData.Domain/AutoMapperProfile.cs
+++ src/WC.Service.PersonalData.Domain/AutoMapperProfile.cs
@@ -9,7 +9,8 @@ public class AutoMapperProfile : Profile
     public AutoMapperProfile()
     {
         CreateMap<PersonalDataModel, PersonalDataEntity>()
-            .ForMember(dest => dest.Role, opt => opt.NullSubstitute("User"));
+            .ForMember(dest => dest.Role,
+                opt => opt.MapFrom(src => string.IsNullOrWhiteSpace(src.Role) ? "User" : src.Role));
 
         CreateMap<PersonalDataEntity, PersonalDataModel>();
     }
